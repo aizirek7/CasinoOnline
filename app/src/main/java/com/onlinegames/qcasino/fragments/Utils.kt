2df05b0b4c436043a7d9cef7cf50180b4c7676b5
Aizirek <adokturbekk@gmail.com>
2022-02-08 16:10:04 +0600

    Initial commit

---
+++ app/src/main/java/com/onlinegames/qcasino/fragments/Utils.kt
@@ -0,0 +1,11 @@
+package com.onlinegames.qcasino.fragments
+
+object Utils {
+    const val ShearedPref = "shearedPref"
+    const val TAG = "TAG"
+    const val KEY_FOR_SP = "key"
+    const val URL = "https://greenslots.ru/WkyDrQSN"
+    const val URL_FROM_URL = "url"
+    const val SCORE = "score"
+    const val IMAGE_URL = "https://disk.yandex.ru/d/S9Ocf8Hzx22Z_g"
+}
